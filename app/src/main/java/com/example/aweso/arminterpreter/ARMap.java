53863b0d3735b396789cea33abaff7dd212ad16a
alexisrolenc <alexis.rolenc@gmail.com>
2018-12-04 12:39:57 -0600

    multiple instruction

---
+++ app/src/main/java/com/example/aweso/arminterpreter/ARMap.java
@@ -0,0 +1,75 @@
+package com.example.aweso.arminterpreter;
+
+import java.util.Arrays;
+
+public class ARMap
+{
+    //Final Fields
+    public static final int MEMORY_SIZE = 64;
+    private static String[] instructions = {"ADD", "SUB"};
+    private static int[] instructionMappings = {1112, 1624};
+    public static Register[] registers = new Register[31];
+    public static int[] memory = new int[ARMap.MEMORY_SIZE];
+
+    public static void init()
+    {
+        //initialize memory
+        Arrays.fill(ARMap.memory, 0);
+
+        for(int i = 0; i < ARMap.registers.length; i++)
+        {
+            ARMap.registers[i] = new Register("X" + i);
+        }
+    }
+
+    public static Register findRegisterWithName(String nameOfRegister)
+    {
+        for(Register r : ARMap.registers)
+        {
+            if(r.getHumanReadableName().equalsIgnoreCase(nameOfRegister))
+            {
+                return r;
+            }
+        }
+        return null;
+    }
+
+    public static BinaryValue lookupInstruction(String instruction)
+    {
+        instruction = instruction.toUpperCase();
+        for(int i = 0; i < instructions.length; i++)
+        {
+            if(instructions[i].equals(instruction))
+            {
+                return new BinaryValue(decimalTo11BitBinary(instructionMappings[i]));
+            }
+        }
+        return null;  //we assume every mapping is found, so this should never get hit!
+    }
+
+    public static String decimalTo5BitBinary(int opCode)
+    {
+        return ARMap.decimalToNBitBinary(opCode, 5);
+    }
+
+    private static String decimalTo11BitBinary(int opCode)
+    {
+        return ARMap.decimalToNBitBinary(opCode, 11);
+    }
+
+    public static String decimalToNBitBinary(long opCode, int numBits)
+    {
+        String answer = "";
+        while(opCode > 0)
+        {
+            answer = (opCode%2) + answer;
+            opCode /= 2;
+        }
+
+        for(int i = answer.length(); i < numBits; i++)
+        {
+            answer = 0 + answer;
+        }
+        return answer;
+    }
+}
---
+++ app/src/main/java/com/example/aweso/arminterpreter/Register.java
@@ -0,0 +1,70 @@
+package com.example.aweso.arminterpreter;
+
+import android.content.Intent;
+
+public class Register
+{
+    private String humanReadableName; //X13
+    private int decimalRegisterNumber; //13
+    private String fiveBitBinaryRegisterNumber;//"1101"
+    private int value; //the current value of stored in the register
+    private static int bitSize = 32;
+
+    public Register(String humanReadableName)
+    {
+        this.humanReadableName = humanReadableName;
+        //"X13" -> "13"
+        this.decimalRegisterNumber = Integer.parseInt(humanReadableName.substring(1));
+        this.fiveBitBinaryRegisterNumber = ARMap.decimalTo5BitBinary(this.decimalRegisterNumber);
+        this.value = 0;
+    }
+
+    private static String flipBits(String bin)
+    {
+        String answer = "";
+        for(int i = 0; i < bin.length(); i++)
+        {
+            answer += bin.charAt(i)=='0'?'1':'0';
+        }
+        return answer;
+    }
+
+    public static String toBinaryString(int value)
+    {
+        if(value < 0)
+        {
+            String bin = ARMap.decimalToNBitBinary(value * -1, Register.bitSize);
+            String flipped = Register.flipBits(bin);
+            //String encoded = ARMap.decimalToNBitBinary(Integer.parseInt(flipped, 2) + 1, Register.bitSize);
+            long l = Long.parseLong(flipped,2);
+            l += 1;
+            String twosComp = ARMap.decimalToNBitBinary(l, Register.bitSize);
+            return twosComp;
+        }
+        else
+        {
+            return ARMap.decimalToNBitBinary(value, Register.bitSize);
+        }
+
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getHumanReadableName() {
+        return humanReadableName;
+    }
+
+    public int getDecimalRegisterNumber() {
+        return decimalRegisterNumber;
+    }
+
+    public String getFiveBitBinaryRegisterNumber() {
+        return fiveBitBinaryRegisterNumber;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
